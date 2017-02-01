4ba6e0a51da34a05115ac41c96c27527f1d3a21a
michaelbadaracca <michael.badaracca@gmail.com>
2017-02-01 00:13:56 -0700

    Added files for SQL Lab #2

---
+++ Java/Assignment #3/AugustineJavaAssn3/src/augustinejavaassn3/AugustineMortgageCalculator.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package augustinejavaassn3;
+
+/**
+ *
+ * @author Michael
+ */
+public class AugustineJavaAssn3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+    /**   
+    A main method to read the inputs from the user, display some values, and call the other methods.
+
+    A method (1) to display a description of what the program will do.
+
+    A method (2) to compute the monthly property tax..
+
+    A method (3) to compute the monthly insurance premium.
+
+    A method (4) to compute the monthly principle and interest loan payment.
+
+    A method (5) to compute and display the results of the program.
+   **/
+    
+}
